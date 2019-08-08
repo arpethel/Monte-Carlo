5abeb4a740ce698b576b04361aabbf8997fb79a7
Pedro Marchante <pmarc560@gmail.com>
2019-08-08 13:27:37 -0400

    created classes

---
+++ Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApproximatingPI
+{
+    struct Point
+    {
+
+        private double x { get; }
+        private double y { get; }
+        public string color;
+
+        public Point(Random r)
+        {
+            this.x = r.NextDouble();
+            this.y = r.NextDouble();
+            this.color = "";
+        }
+
+        public override string ToString()
+        {
+            return color;
+        }
+    }
+}
