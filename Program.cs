8e5baec8754e2b1c080373def2d0bd0acdb81d5c
Andrew Pethel <arpethel@gmail.com>
2019-08-08 13:21:35 -0400

    Project setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Monte_Carlo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
